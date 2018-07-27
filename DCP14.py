dd5eaf2124d42392533167cced5e5e5ea4c689f1
atrinh3 <36494242+atrinh3@users.noreply.github.com>
2018-07-27 10:13:39 -0700

    Update DCP14.py

--- DCP14.py
+++ DCP14.py
@@ -1,3 +1,18 @@
+# Daily Coding Problem # 14
+#
+# The area of a circle is defined as πr^2. 
+# Estimate π to 3 decimal places using a Monte Carlo method.
+# Hint: The basic equation of a circle is x2 + y2 = r2.
+#
+# Generate a bunch of number pairs from 0 - 1 (x, y coordinates)
+# For each generated point, determine if the point lies in the
+# upper right quadrant of the unit circle (0 <= x & y <= 1)
+# Take the ratio of the number of points in the circle and the 
+# total number of random points.  Multiply by 4 to complete the
+# remaining quadrants of the unit circle.  The result should be
+# an approximation of pi.  The higher n is, the more accurate the
+# approximation is.
+
 import random as r
 import math
 
